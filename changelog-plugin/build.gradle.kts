b16fe5c21049c6f6b926042a22bc37fbfa7906de
Alexander Goussas <alexandergoussas.thoughtworks@latam.com>
2025-10-27 18:04:23 -0500

    feat: configure publish plugin

--- changelog-plugin/build.gradle.kts
+++ changelog-plugin/build.gradle.kts
@@ -1,18 +1,30 @@
 plugins {
     `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.2.1"
+    signing
+    id("com.gradleup.shadow") version "9.2.2"
     kotlin("jvm") version "2.1.21"
 }
 
 group = "io.github.aloussase"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
 }
 
+
+
 gradlePlugin {
+    website = "https://github.com/aloussase/gradle-changelog-plugin"
+    vcsUrl = "https://github.com/aloussase/gradle-changelog-plugin.git"
+
     plugins {
         create("changelog") {
             id = "$group.$name"
+            displayName = "Plugin for generating CHANGELOG entries"
+            description = "Generate a CHANGELOG automatically from Git commit history"
+            tags = listOf("git", "changelog", "documentation", "tool")
             implementationClass = "$group.$name.ChangelogPlugin"
         }
     }
@@ -26,4 +38,8 @@ dependencies {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+
+tasks.withType<Jar>().configureEach {
+    archiveClassifier.set("")
 }
